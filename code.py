eca53bbc1771116bbdd0a591918603c702bd6249
elenamii <elenagholami@gmx.de>
2025-11-08 21:51:05 +0100

    add polynom addition

--- code.py
+++ code.py
@@ -1,37 +1,21 @@
 
+#stützstellen und -werte werden abgefragt und gespeichert 
+anzahl = int(input("Gewünschte ANZAHL Stützstellen eingeben: ")) #0 handeling fehlt
+x_wert = []
+y_wert = []
 
-# anzahl = int(input("Gewünschte ANZAHL Stützstellen eingeben: ")) #0 handeling
-# x_wert = []
-# y_wert = []
+print("Bitte geben Sie ihre gewünschten Stützstellen mit ihren Stützwerten an.")
 
-# print("Bitte geben Sie ihre gewünschten Stützstellen mit ihren Stützwerten an.")
+for i in range(anzahl):
+    x= float(input("x[i]: ")) # andere schreibweise für index !!
+    y= float(input("y(i): "))
+    x_wert.append(x)
+    y_wert.append(y)
 
-# for i in range(anzahl):
-#     x= float(input("x[i]: ")) # andere schreibweise für index !!
-#     y= float(input("y(i): "))
-#     x_wert.append(x)
-#     y_wert.append(y)
 
 #polynom adition
-#def polynom_add(p,q):
-    #max_length= max(len(p),len(q))
-    #result = [0] * max_length
-    
-    # for i in range(max_length):
-        #if i < len(p):
-            #result[i]+= p[i]
-        #if i < len(q):
-            #result[i] += q[i]
-    #return result
-
-        #a = p[i] if i < len(p) else 0
-        #b = q[i] if i < len(q) else 0
-        #result = a+b
-
-p=[1,2,3]
-q=[5,6]
-def add(p,q):
-	ergebnis=[]
+def poly_add(p,q):
+	result=[]
 	max_length= max(len(p), len(q))
 	for i in range(max_length):
 		try:
@@ -43,30 +27,17 @@ def add(p,q):
 		except IndexError:
 			b = 0
 			
-		ergebnis.append(a+b)
+		result.append(a+b)
 			
-	return ergebnis
+	return result
 
-
-	
-print(add(p,q))
-
-
-
-
-
-
-
-# p = [1,2,3]
-# q = [5,6]
-
-#print(polynom_add(p,q)) #expect: [6, 8, 3]
+#test poy_add mit bsp-polynomen 
+p = [1,2,3]
+q = [5,6]	
+print(poly_add(p,q)) #expect: [6, 8, 3]
 
 
 
 
-# p= [1,2,3,4] --> 1*x^0 + 2*x^1 + 3*x^2 + 4*x^3
-# q = [0,9,8] --> 0*x^0 + 9x^1 + 8*x^2
 
-# p(x) + q(x) = (1+0)*x^0 + (2+9)*x^1 + (3+8)*x^2 + (4+7)*x^3
-#             = (1+0) + (2+9)x + (3+8)x^2 + (4+7)x^3
+
