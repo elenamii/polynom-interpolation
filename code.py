a4bc3dff06708d6b97761006698c3751047fa4e3
Paula Kesper <pkesper@icloud.com>
2025-11-08 11:41:50 +0100

    erstesbisschencode die zweite

--- code.py
+++ code.py
@@ -1,3 +1,3 @@
-print("Bitte geben Sie ihre gewünschten Stützstellen mit ihren Stützwerten an.")
-print("Gewünschte ANZAHL Stützstellen eingeben:")
-anzahl = int(input())
+#print("Bitte geben Sie ihre gewünschten Stützstellen mit ihren Stützwerten an.")
+#print("Gewünschte ANZAHL Stützstellen eingeben:")
+#anzahl = int(input())
