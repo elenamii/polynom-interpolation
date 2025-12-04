93e983894065e85e179ab7fc562b9a40379673b6
Paula Kesper <pkesper@icloud.com>
2025-12-04 22:49:08 +0100

    JUNGS JUNGS JUNGS JUNGS ED SOGJRSDKLUG:JS FUNKTIONIERT

--- Manuel_NEUER_VERSUCH/io_input.py
+++ Manuel_NEUER_VERSUCH/io_input.py
@@ -1,10 +1,25 @@
-from interpolation import interpolate
+from polynom import Polynom
 
-# Beispiel für Lagrange:
-basis, P, extra = interpolate("lagrange", x_wert, [y[0] for y in y_wert])
+def einlesen_stuetzstellen():
+    n = int(input("Wie viele Stützstellen möchten Sie eingeben? "))
+    x_wert = []
+    y_wert = []
+    for i in range(n):
+        x = float(input(f"x_{i} = "))
+        x_wert.append(x)
+        y_eingabe = input(f"y_{i} oder y_{i}, y'_{i}, ... bei Ableitungen: ")
+        y_liste = [float(val) for val in y_eingabe.split(",")]
+        y_wert.append(y_liste)
+    return x_wert, y_wert
 
-# Beispiel für Newton:
-basis, P, extra = interpolate("newton", x_wert, [y[0] for y in y_wert])
+def horner(poly: Polynom, x: float) -> float:
+    result = 0
+    for coef in reversed(poly.coeffs):
+        result = result * x + coef
+    return result
 
-# Beispiel für Hermite:
-basis, P, extra = interpolate("hermite", x_wert, y_wert)
+def ausmultiplizieren_linearfaktoren(faktoren: list[Polynom]) -> Polynom:
+    P = Polynom([1])
+    for f in faktoren:
+        P = P * f
+    return P
