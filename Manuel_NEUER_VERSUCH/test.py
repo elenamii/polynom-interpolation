6d64718dcd4006b7155cadec8cf0d33b38235059
Paula Kesper <pkesper@icloud.com>
2025-12-05 13:50:04 +0100

    multi für eleni

--- Manuel_NEUER_VERSUCH/test.py
+++ Manuel_NEUER_VERSUCH/test.py
@@ -7,8 +7,8 @@ from hermite import hermite_interpolation
 
     
 def test_polynom_mult():
-    p1 = Polynom([1, 2])   # 1 + 2x
-    p2 = Polynom([3, 4])   # 3 + 4x
+    p1 = Polynom([2, 3])   # 1 + 2x
+    p2 = Polynom([-3, 1])   # 3 + 4x
     result = p1 * p2       # sollte 3 + 10x + 8x² ergeben
     print(result)
 test_polynom_mult()
