c81c0a77f07509069207487356a4561cff6df1e3
elenamii <elenagholami@gmx.de>
2025-12-06 09:56:15 +0100

    add test function for poly_mul

--- Manuel_NEUER_VERSUCH/test.py
+++ Manuel_NEUER_VERSUCH/test.py
@@ -12,3 +12,17 @@ def test_polynom_mult():
     result = p1 * p2       # sollte 3 + 10x + 8x² ergeben
     print(result)
 test_polynom_mult()
+
+
+#zum eigenständigen testen der multiplikationsfunktion (muss man händisch im code eingeben)
+from polynom import MathWithPolynomials  
+
+
+p = [2, 3, 1]      
+q = [-3, 1]         
+
+result = MathWithPolynomials.poly_mul(p, q)
+
+print("p =", MathWithPolynomials.print_poly_schoen(p))
+print("q =", MathWithPolynomials.print_poly_schoen(q))
+print("p * q =", MathWithPolynomials.print_poly_schoen(result))
